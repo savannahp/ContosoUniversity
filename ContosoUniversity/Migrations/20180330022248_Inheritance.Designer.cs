64e1b7796eb25eb7e3d127528230732fdc98dbc6
Savannah Pyle <bea12022@byui.edu>
2018-03-29 20:37:58 -0600

    step9

--- ContosoUniversity/Migrations/20180330022248_Inheritance.Designer.cs
+++ ContosoUniversity/Migrations/20180330022248_Inheritance.Designer.cs
@@ -13,9 +13,10 @@ using System;
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20180330022248_Inheritance")]
+    partial class Inheritance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
